cf799499d1bdc220893926eec085bedece1d3e93
Ethan Nelson <git@ethan-nelson.com>
2017-04-10 21:58:42 -0500

    Include Heroku Procfile

--- site.py
+++ site.py
@@ -14,6 +14,7 @@ import os
 app = Flask(__name__)
 try:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = 'itsatrap'
 except:
     raise Exception('Server settings not set in environmental variable `DATABASE`.')
 
@@ -326,6 +327,5 @@ def admin():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.config['SECRET_KEY'] = 'itsatrap'
     app.run(host='0.0.0.0', port=5432)
